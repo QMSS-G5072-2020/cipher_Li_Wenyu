49d0d618ca24f0295a356b13695340a29d998f96
Wenyu <71299532+Irene-2020@users.noreply.github.com>
2020-11-16 00:52:42 +0800

    Test added

--- tests/test_cipher_wl2788.py
+++ tests/test_cipher_wl2788.py
@@ -3,3 +3,8 @@ from cipher_wl2788 import cipher_wl2788
 
 def test_version():
     assert __version__ == '0.1.0'
+    
+def test_cipher_with_single_word():
+    expected = "ibqqz"
+    actual = cipher_wl2788.cipher("happy", 1, True)
+    assert actual == expected
